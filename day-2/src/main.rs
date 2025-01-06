a1a251590044fbdbc539d2ebb5cb7dfcb38ba3ff
frankie-powers <franklin.power21@gmail.com>
2025-01-06 21:46:32 +0100

    removed target folder

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-
-    x = 6;
-    println!("The value of x is: {}", x);
+//constants
+const MAX_POINTS:i32 = 100_000;
+println!("The value of MAX_POINTS is {}",MAX_POINTS);
 }
