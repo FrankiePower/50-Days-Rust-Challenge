575ea35599bd2e3002acef34ad67c920af57e71b
frankie-powers <franklin.power21@gmail.com>
2025-02-12 21:44:41 +0100

    day 6 done

--- day-6/variables_and_mutability/src/main.rs
+++ day-6/variables_and_mutability/src/main.rs
@@ -11,4 +11,18 @@ fn main() {
     }
 
     println!("The value of x is: {x}");
+
+    print_labeled_measurement(5, 'h');
+
+    let x = five();
+
+    println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
 }
