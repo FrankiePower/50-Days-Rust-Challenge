e1136ebb65ea3e28655ed0fd4f29ca91ebf9161b
frankie-powers <franklin.power21@gmail.com>
2025-02-15 06:17:54 +0100

    ownership done

--- day-7/ownership/src/main.rs
+++ day-7/ownership/src/main.rs
@@ -8,7 +8,38 @@ fn read(y: bool) {
     }
 }
 
+// memory is the space where data is stored during the execution of a program.
 fn main() {
-    read(x); // This causes undefined behaviour as x is not defined in the scope of main function.
-    let x = true;
+    /*   read(x); // This causes undefined behaviour as x is not defined in the scope of main function.
+    let x = true; */
+    /*
+    let n = 5;
+    let y = plus_one(n);
+    println!("The value of y is: {y}");
+
+    let a_num = 4;
+    make_and_drop(); */
+
+    let first = String::from("Ferris");
+    let full = add_suffix(first);
+    println!("{full}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn make_and_drop() {
+    let a_box = Box::new(5);
+}
+
+fn add_suffix(mut name: String) -> String {
+    name.push_str(" Jr.");
+    name
+}
+
+fn move_a_box(b: Box<i32>) {
+    let b = Box::new(0);
+    let b2 = b;
+    move_a_box(b);
 }
